12ff846119d0d305667ccb47b98c246d3a0cba57
Saurabh Purnaye <spurnaye@gmail.com>
2015-07-15 10:58:55 -0400

    down to 4 errors and 0 failures

--- test/pay_with_amazon_unit_test.rb
+++ test/pay_with_amazon_unit_test.rb
@@ -24,7 +24,6 @@ class PayWithAmazonUnitTest < Minitest::Test
 
   HEADERS = {
     'Accept'=>'*/*',
-    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
     'User-Agent'=>"Language=Ruby; ApplicationLibraryVersion=#{PayWithAmazon::VERSION}; Platform=#{RUBY_PLATFORM}; MWSClientVersion=#{PayWithAmazon::API_VERSION}; ApplicationName=; ApplicationVersion="
   }
 
